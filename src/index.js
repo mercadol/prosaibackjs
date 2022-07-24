2deca34ed738129b33b49f6f57144c0572fc3dcd
Leonardo Mercado <lamercadob1@gmail.com>
2022-07-24 01:41:36 -0500

    cambio menor

--- src/index.js
+++ src/index.js
@@ -1,7 +1,34 @@
 const express = require('express');
 
 const app= express();
+const port =3000
 
-app.listen(8000, ()=>{
-    console.log("servidor corriendo 8000");
+app.get('/', (req, res) =>{
+    res.send('hola');
+});
+
+app.get('/nuevaruta', (req, res) =>{
+    res.send('hola soy un nuevo endpoint');
+});
+
+app.get('/products', (req, res) =>{
+    res.json([{
+        nombre: "producto 1",
+        precio: 2000
+
+    }]);
+});
+
+app.get('/products/:id', (req, res) =>{
+    const {id} = req.params;
+    res.json({
+        nombre: "producto 1",
+        precio: 2000
+
+    });
+});
+
+
+app.listen(port, ()=>{
+    console.log(`servidor corriendo ${port}`);
 });
